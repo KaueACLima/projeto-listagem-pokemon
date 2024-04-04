26cdcbef676423ca618068f6fbcd23ef5bf54007
Kauê Lima <kaue5939.lima@gmail.com>
2024-04-04 03:39:53 -0300

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+  const modoEscuroEstaAtivado = body.classList.contains("modo-escuro");
+
+  body.classList.toggle("modo-escuro");
+
+  if (modoEscuroEstaAtivado) {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./assets/img/sun.png");
+  } else {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./assets/img/moon.png");
+  };
+});
